4292d5147f2632b6cb945fa5327ba5ed9fea0f0d
Lareta064 <lareta1@yandex.ru>
2025-06-02 20:47:16 +0300

    header-mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let bodyEl = document.body;
+   let bodyEl = document.body;
+    	/*open mobile menu */
+    const menuButton = document.querySelector('#menu-toggle');
+    const mobileMenu = document.querySelector('#header-mobile-menu');
+    
+    menuButton.addEventListener('click', ()=> {
+      
+      if( menuButton.classList.contains('active')){
+        menuButton.classList.remove('active');
+        mobileMenu.classList.remove('active');
+        bodyEl.classList.remove('lock');
+        
+      }else{
+        menuButton.classList.add('active');
+        mobileMenu.classList.add('active');
+        bodyEl.classList.add('lock');
+      }
+    });
    
     //PROMO SLIDER
     let promoSlider = new Swiper('.promo-swiper',{
